d114b33a7820cea090f8883b750c84adabdd26e4
bror <reachingbror@gmail.com>
2026-02-06 13:30:29 +0100

    Working with flex on my menu and trying diffrent slots for logos. Work in progress. Saving changes

--- src/components/BoatStart.vue
+++ src/components/BoatStart.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="flex">
     <Menu :model="menuItems" class="w-64 h-screen">
+      <template #start>
+        
+        <div class="my-logo-container">
+          <img src="/ifboatylogo5.png" alt="Logo" class="logo"/>
+        </div>
+      </template>
+
       <template #item="{ item, props }">
+        <div class="flex justify-center pr-4">
         <router-link #="{ href, navigate, isExactActive }" :to="{ name: item.route, params: { id: boatId } }" custom>
           <a :href="href" v-bind="props.action" @click="navigate" :class="{ 'active-menu-item': isExactActive }">
-            <span class="ml-2">{{ item.label }}</span>
+          
+            <span class="flex justify-right">{{ item.label }}</span>
+            <span :class="item.icon"  />
+            
           </a>
         </router-link>
+        </div>
       </template>
     </Menu>
     <div class="flex-1 p-8">
@@ -20,6 +32,9 @@
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import type { MenuItem } from 'primevue/menuitem'
+import { icon } from '@primeuix/themes/aura/avatar'
+import { IconField } from 'primevue'
+
 
 const route = useRoute()
 const boatId = computed(() => route.params.id)
@@ -27,15 +42,18 @@ const boatId = computed(() => route.params.id)
 const menuItems: MenuItem[] = [
   {
     label: 'Översikt',
+    icon:  'pi pi-home',
     route: 'boat.overview',
   },
   {
     label: 'Inventory',
     route: 'boat.inventory',
+    icon:  'pi pi-cog',
   },
    {
     label: 'Todo',
     route: 'boat.todo',
+    icon: 'picheck-square',
   }
 ]
 </script>
@@ -45,5 +63,20 @@ const menuItems: MenuItem[] = [
   background: rgba(255, 255, 255, 0.15);
   color: white;
   font-weight: 600;
+}
+
+.my-logo-container {
+  padding: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+  margin-bottom: 1rem;
+}
+
+.logo {
+  max-width: 100%;
+  height: auto;
+  max-height: 80px;
 }
 </style>
