5476002d428ec62b08cd73196f54d96674b7f338
bror <reachingbror@gmail.com>
2026-02-10 12:11:24 +0100

    Added button at the end of menu to navigate without adressbar.

--- src/components/BoatStart.vue
+++ src/components/BoatStart.vue
@@ -2,25 +2,36 @@
   <div class="flex">
     <Menu :model="menuItems" class="w-64 h-screen">
       <template #start>
-        
         <div class="my-logo-container">
-          <img src="/ifboatylogo5.png" alt="Logo" class="logo"/>
+          <img src="/ifboatylogo5.png" alt="Logo" class="logo" />
         </div>
       </template>
 
       <template #item="{ item, props }">
-
-        <router-link #="{ href, navigate, isExactActive }" :to="{ name: item.route, params: { id: boatId } }" custom>
-          <a :href="href" v-bind="props.action" @click="navigate" :class="{ 'active-menu-item': isExactActive }" class="grid-cols-[auto_1_fr]">
-          <span :class="item.icon"  />
-            <span >{{ item.label }}</span>
-           
-         
+        <router-link
+          #="{ href, navigate, isExactActive }"
+          :to="{ name: item.route, params: { id: boatId } }"
+          custom
+>
+          <a
+            :href="href"
+            v-bind="props.action"
+            @click="navigate"
+            :class="{ 'active-menu-item': isExactActive }"
+            class="grid-cols-[auto_1_fr]"
+          >
+            <span :class="item.icon" />
+            <span class="px-2 py-2">{{ item.label }}</span>
           </a>
         </router-link>
-
+      </template>
+      <template #end>
+        <div class=" flex px-2 py-2 mt-1">
+<Button  label="Back to start" severity="info" />
+</div>
       </template>
     </Menu>
+
     <div class="flex-1 p-8">
       <!-- Mitt content till höger om menyn -->
       <router-view></router-view>
@@ -35,31 +46,35 @@ import type { MenuItem } from 'primevue/menuitem'
 import { icon } from '@primeuix/themes/aura/avatar'
 import { IconField } from 'primevue'
 
-
 const route = useRoute()
 const boatId = computed(() => route.params.id)
 
 const menuItems: MenuItem[] = [
   {
     label: 'Översikt',
-    icon:  'pi pi-home',
+    icon: 'pi pi-home',
     route: 'boat.overview',
   },
   {
     label: 'Inventory',
-    icon:  'pi pi-database',
+    icon: 'pi pi-database',
     route: 'boat.inventory',
   },
-   {
+  {
     label: 'Todo',
-    icon:  'pi pi-check-square',
+    icon: 'pi pi-check-square',
     route: 'boat.todo',
   },
   {
     label: 'Min båt',
-    icon:  'pi pi-cog',
+    icon: 'pi pi-cog',
     route: 'boat.myboat',
-  }
+  },
+  {
+    separator: true,
+    class: 'mt-4',
+  },
+ 
 ]
 </script>
 
