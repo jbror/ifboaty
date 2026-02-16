5a65d1d512988c7325115797f6d9f9c8dda08ce9
bror <reachingbror@gmail.com>
2026-02-16 17:08:37 +0100

    removing old temp-component

--- components.d.ts
+++ components.d.ts
@@ -15,7 +15,6 @@ declare module 'vue' {
     BoatStart: typeof import('./src/components/BoatStart.vue')['default']
     Button: typeof import('primevue/button')['default']
     Column: typeof import('primevue/column')['default']
-    copy: typeof import('./src/components/Todo copy.vue')['default']
     DataTable: typeof import('primevue/datatable')['default']
     Dialog: typeof import('primevue/dialog')['default']
     InputNumber: typeof import('primevue/inputnumber')['default']
