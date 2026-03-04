634a4de3b8c4ff1dafdae38d8cd57924308ff551
bror <reachingbror@gmail.com>
2026-03-04 14:14:55 +0100

    Updated my theme and preset to use the noir design.

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import './styles.css'
 import Aura from '@primevue/themes/aura'
 
 import Nora from '@primevue/themes/nora'
- import Lara from '@primevue/themes/lara'
- import Material from '@primevue/themes/material'
+import Lara from '@primevue/themes/lara'
+import Material from '@primevue/themes/material'
 import { definePreset } from '@primevue/themes'
 import 'primeicons/primeicons.css'
 
@@ -17,34 +17,46 @@ const app = createApp(App)
 const myTheme = definePreset(Aura, {
   semantic: {
     primary: {
-      50: '{sky.50}',
-      100: '{sky.100}',
-      200: '{sky.200}',
-      300: '{sky.300}',
-      400: '{sky.400}',
-      500: '{sky.500}',
-      600: '{sky.600}',
-      700: '{sky.700}',
-      800: '{sky.800}',
-      900: '{sky.900}',
-      950: '{sky.950}',
+      50: '{zinc.50}',
+      100: '{zinc.100}',
+      200: '{zinc.200}',
+      300: '{zinc.300}',
+      400: '{zinc.400}',
+      500: '{zinc.500}',
+      600: '{zinc.600}',
+      700: '{zinc.700}',
+      800: '{zinc.800}',
+      900: '{zinc.900}',
+      950: '{zinc.950}',
     },
-  },
-  colorScheme: {
-    light: {
-      highlight: {
-        background: '{sky.50}',
-        focusBackground: '{sky.100}',
-        color: '{sky.700}',
-        focusColor: '{sky.800}',
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{zinc.950}',
+          inverseColor: '#ffffff',
+          hoverColor: '{zinc.900}',
+          activeColor: '{zinc.800}',
+        },
+        highlight: {
+          background: '{zinc.950}',
+          focusBackground: '{zinc.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff',
+        },
       },
-    },
-    dark: {
-      highlight: {
-        background: '{sky.950}',
-        focusBackground: '{sky.900}',
-        color: '{sky.100}',
-        focusColor: '{sky.50}',
+      dark: {
+        primary: {
+          color: '{zinc.50}',
+          inverseColor: '{zinc.950}',
+          hoverColor: '{zinc.100}',
+          activeColor: '{zinc.200}',
+        },
+        highlight: {
+          background: 'rgba(250, 250, 250, .16)',
+          focusBackground: 'rgba(250, 250, 250, .24)',
+          color: 'rgba(255,255,255,.87)',
+          focusColor: 'rgba(255,255,255,.87)',
+        },
       },
     },
   },
