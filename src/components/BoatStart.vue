6a4c6edfb1d2877381574fd11ed9bc22b9f8b545
bror <reachingbror@gmail.com>
2026-04-21 19:04:59 +0200

    improving baselayout

--- src/components/BoatStart.vue
+++ src/components/BoatStart.vue
@@ -10,8 +10,8 @@
     <div class="flex flex-1">
       <Drawer v-model:visible="sidebarVisible" class="md:hidden w-64!"> </Drawer>
 
-      <div class="flex flex-1 w-full overflow-hidden h-screen">
-        <Menu :model="menuItems" class="hidden md:block w-56 md:top-0 md:self-start md:min-h-screen md:max-h-screen">
+      <div class="flex flex-1 w-full ">
+        <Menu :model="menuItems" class="hidden md:block w-56 md:top-0 md:self-start md:min-h-screen h-full">
           <template #start>
             <div class="h-38 mt-4 mb-4 mr-12">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="100%" height="100%" preserveAspectRatio="xMidYMid meet">
@@ -60,7 +60,7 @@
           </template>
         </Menu>
 
-        <div class="flex flex-col flex-1 overflow-y-auto p-2">
+        <div class="flex flex-col flex-1 overflow-y-auto p-2 ">
           <div class="bg-white rounded-xl shadow-sm min-h-full p-2 max-w-4xl">
             <router-view></router-view>
           </div>
